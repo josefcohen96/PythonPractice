9971faf1cbb11c99dbf3b9e15dbfe7ab4d1eda08
josefcohen96 <josef244@gmail.com>
2025-08-10 13:52:56 +0300

    deleteing

--- devices/base/types.py
+++
@@ -1,5 +0,0 @@
-from typing import Literal
-
-DeviceState = Literal["connected", "disconnected", "busy", "fault"]
-ReadKey  = Literal["voltage", "current", "temp", "output"]
-SetKey   = Literal["voltage", "current_limit", "output", "power"]
